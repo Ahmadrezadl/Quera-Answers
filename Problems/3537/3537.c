d65af619d3d9f8ebbcdc9681778c8c2ed9fe2677
Ahmadrezadl <Ahmadrezakamali121@yahoo.com>
2019-11-06 11:19:53 +0330

    Add 3537

---
+++ Problems/3537/3537.c
@@ -0,0 +1,15 @@
+//https://quera.ir/problemset/contest/3537/
+//Solve by: Ahmadrezadl
+#include <stdio.h>
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	printf("W");
+	while(x--)
+	{
+		printf("o");
+	}
+	printf("w!");
+}
+
