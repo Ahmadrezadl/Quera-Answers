a905d4154b5704fcd59278fa0f7c650fbf79ec69
Ahmadrezadl <Ahmadrezakamali121@yahoo.com>
2019-11-06 11:29:17 +0330

    Add 2636

---
+++ Problems/2636/2636.c
@@ -0,0 +1,10 @@
+//https://quera.ir/problemset/contest/2636/
+//Solve by: Farnood Setoode
+#include <stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+	printf("%d %d %d %d %d %d",1-a,1-b,2-c,2-d,2-e,8-f);
+}
+
