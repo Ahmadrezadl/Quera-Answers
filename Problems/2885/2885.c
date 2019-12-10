26390b48efd7b93b9ff0b2afe795db188332427e
kasrazarei39 <kasrazarei39@gmail.com>
2019-12-10 07:38:31 -0800

    add 2885

---
+++ Problems/2885/2885.c
@@ -0,0 +1,19 @@
+//https://quera.ir/problemset/contest/2885
+//Solved by: kasrazarei39
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int a;
+        int i=1;
+        scanf("%d",&a);
+            while(i<=a)
+            {
+                printf("man khoshghlab hastam\n");
+                i++;
+            }
+
+
+        return 0;
+}
