d91d8b3315f89d0b12679acd9bb1efcfb87b56a6
kasrazarei39 <kasrazarei39@gmail.com>
2019-12-11 21:23:46 -0800

    add 589

---
+++ Problems/589/589.c
@@ -0,0 +1,19 @@
+//https://quera.ir/problemset/university/589
+//Solved by: kasrazarei39
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i=1;
+    int a=1;
+        scanf("%d",&n);
+        while(i<=n){
+            a=a*i;;
+            i++;
+
+        }
+        printf("%d",a);
+
+    return 0;
+}
