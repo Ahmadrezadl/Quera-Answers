8936722bed709fe596e237a73acd9d0b5e0a441d
kasrazarei39 <kasrazarei39@gmail.com>
2020-01-03 00:19:20 -0800

    add 10162

---
+++ Problems/10162/10162.c
@@ -0,0 +1,18 @@
+//https://quera.ir/problemset/contest/10162
+//Solved by: kasrazarei39
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("Bala Barare");
+    }
+    else
+    {
+        printf("Payin Barare");
+    }
+    return 0;
+}
